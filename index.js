6544c0b44675f577fc268af9246f08d2b90825b9
Julien Ergan <julien.ergan.code@gmail.com>
2020-08-21 10:58:55 +0900

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+// const connect = mongoose.connect(url);
+// const connect = mongoose.connect(url, { useNewUrlParser: true });
+const connect = mongoose.connect(url, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true, 
+    useFindAndModify: false 
+})
+
+connect.then((db) => {
+    console.log('Connect correctly to server');
+
+    var newDish = Dishes({
+        name: 'Name test 1',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+            return Dishes.deleteMany({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
